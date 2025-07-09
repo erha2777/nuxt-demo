f98ac02d01194bb5ff2c31122397aa655d727617
erha2777 <2777840869@qq.com>
2025-07-09 23:13:16 +0800

    feat: nuxt视图、布局、组件、路由初体验

---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    这是头部
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
